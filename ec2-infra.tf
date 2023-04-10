823145939ddb85c4d993dca45ab056db2dd06b75
ShinDongJun <wq0212@naver.com>
2023-04-10 17:22:07 +0900

    feat: study about variable & output

--- ec2-infra.tf
+++ ec2-infra.tf
@@ -1,8 +1,29 @@
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
 resource "aws_instance" "ubuntu_server" {
   ami           = "ami-04cebc8d6c4f297a3" # Ubuntu 22.04 LTS
   instance_type = "t2.nano"
 
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "hello,world" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
   tags = {
-    Name = "Example-EC2-Linux-Instance"
+    Name = "terraform-example"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "server_port" {
+  type        = number
+  default     = 8080
+  description = "The port th server will use for HTTP requests"
+}
